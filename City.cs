9df60d02d3956e33569aff109c750f5021e45b13
Sergey <thechilavek@gmail.com>
2018-05-16 17:39:52 +0300

    Initial commit

---
+++ City.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MVVMWeather
+{
+    class City: INotifyPropertyChanged
+    {
+        string name;
+        int id;
+        public string Name { get { return name; }
+        set { name = value;
+                OnPropertyChanged("Name");} }
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+    class MainModel: INotifyPropertyChanged
+    {
+        public MainModel()
+        {
+            CityList = new ObservableCollection<City>
+            {
+                new City { Name="Брянск", Id = 1},
+                new City {Name="Москва", Id = 11 },
+                new City {Name="Казань", Id = 21 },
+                new City {Name="Курск", Id = 10 }
+            };
+        }
+        private City selectedCity;
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set { selectedCity = value;
+                OnPropertyChanged("SelectedCity");}
+        }
+
+        public ObservableCollection<City> CityList { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+}
---
+++ ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using System.Collections.ObjectModel;
+
+namespace MVVMWeather
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private MainModel model = new MainModel();
+        public ObservableCollection<City> CityList2 => model.CityList;
+
+
+
+        private City selectedCity;
+        public ObservableCollection<City> CityList { get; set; }
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public ViewModel()
+        {
+            CityList = new ObservableCollection<City>
+            {
+                new City { Name="Брянск", Id = 1},
+                new City {Name="Москва", Id = 11 },
+                new City {Name="Казань", Id = 21 },
+                new City {Name="Омск", Id = 10 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
